a37d7c2ec91844619ba746330f5ee5d1003933e5
Maximka Akkuratnyy <mdakkuratnyy@gmail.com>
2024-01-15 17:53:54 +0300

    error commit

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {delay, Observable, throwError} from "rxjs";
+import {IProduct} from "../models/product";
+
+@Injectable({
+  providedIn:'root'
+})
+export class ProductsService{
+  constructor(private http:HttpClient) {
+  }
+  getAll():Observable<IProduct[]>{
+    return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
+      // params: new HttpParams().append('limit',5)
+    }).pipe(
+      delay(1500),
+      catchError(this.errorHandler)
+    )
+
+  }
+  private errorHandler(HttpErrorResponse){
+    return throwError(() => error.message())
+  }
+}
