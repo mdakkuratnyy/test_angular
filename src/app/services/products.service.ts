463bc936791f3011347f7f9777d3cf7aa4cf8ed4
Maximka Akkuratnyy <mdakkuratnyy@gmail.com>
2024-01-15 18:06:18 +0300

    second commit

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,13 +1,18 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {delay, Observable, throwError} from "rxjs";
+import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
+import {catchError, delay, Observable, throwError} from "rxjs";
 import {IProduct} from "../models/product";
+import {ErrorService} from "./error.service";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Injectable({
   providedIn:'root'
 })
 export class ProductsService{
-  constructor(private http:HttpClient) {
+  constructor(
+    private http:HttpClient,
+    private errorService: ErrorService
+    ) {
   }
   getAll():Observable<IProduct[]>{
     return this.http.get<IProduct[]>('https://fakestoreapi.com/products', {
@@ -18,7 +23,8 @@ export class ProductsService{
     )
 
   }
-  private errorHandler(HttpErrorResponse){
-    return throwError(() => error.message())
+  private errorHandler(error:HttpErrorResponse){
+    this.errorService.handle(error.message)
+    return throwError(() => error.message)
   }
 }
